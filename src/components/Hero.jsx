2b61aef172a8d47ad4710245819cb84e85391b50
Kris-05 <vjkrisna995@gmail.com>
2025-09-20 16:53:07 +0530

    Update Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -92,7 +92,7 @@ const Hero = () => {
       )}
 
       <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
-        <div>
+        {/* <div>
           <div className='mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
             <div 
               onClick={handleVideoChange} 
@@ -125,6 +125,42 @@ const Hero = () => {
             muted
             className='absolute left-0 top-0 size-full object-cover object-center'
             onLoadedData={handleVideoLoad}
+          />
+        </div> */}
+
+        <div>
+          <div className='mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
+            <div 
+              onClick={handleVideoChange} 
+              className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100'
+            >
+              <img
+                ref = {nxtVideoRef}
+                src = {getVideoSrc((currIndex % totalVideos) + 1)}
+                // loop
+                // muted
+                id = "current-video"
+                className='size-64 origin-center scale-150 object-center object-cover'
+                onLoad={handleVideoLoad}
+              />
+            </div>
+          </div>
+          <img
+            ref = {nxtVideoRef}
+            src = {getVideoSrc(currIndex)}
+            // loop
+            // muted
+            id = "next-video"
+            className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+            onLoad={handleVideoLoad}
+          />
+          <img
+            src = {getVideoSrc(currIndex === totalVideos - 1 ? 1 : currIndex)}
+            autoPlay
+            // loop
+            // muted
+            className='absolute left-0 top-0 size-full object-cover object-center'
+            onLoad={handleVideoLoad}
           />
         </div>
 
